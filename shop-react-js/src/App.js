7468fd027c7526de0035d54b8e74922a64d06192
Daria <shkatov.pad@gmail.com>
2024-10-26 18:28:59 +0300

    the ultimate commit

--- shop-react-js/src/App.js
+++ shop-react-js/src/App.js
@@ -9,33 +9,6 @@ import ProductCart from './SHOP/ProductCart';
 
 
 function App() {
-
-  // state = { products: }; []
-  // componentDidMount() {
-  //   fetch('./products.json')
-  //     .then((responce) => responce.json())
-  //     .then((data) => this.setState({ products: Object.values(data) }));
-  // }
-  // render() {
-  //   const { products } = this.state;
-  //   return (
-  //     <main className="container content" >
-  //       <Products products={products} />
-  //     </main>
-  //   );
-  // }
-
-
-  // <BrowserRouter>
-  //   <Link to="/">Home</Link >
-  //   <Link to="/about">About</Link >
-  //   <Routes>
-  //     <Route path="/" element={<Home />} />
-  //     <Route path="/about" element={<About />} />
-  //   </Routes>
-
-  // </BrowserRouter>
-
   return (
     <div className="App">
 
